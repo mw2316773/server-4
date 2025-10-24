baa4d11fa190c2abe682312c3ab9843ce1a5bbb6
mw2316773 <206798926+mw2316773@users.noreply.github.com>
2025-10-24 12:09:27 +0900

    第5回授業

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // 自分で一から書いてみよう
+// Honoフレームワークの読み込み
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+const app = new Hono();
+
+// publicフォルダー以下のファイルをウェブサイトとみなす
+app.use('/*', serveStatic({ root: './public' }));
+
+// GETリクエストに対する処理
+app.get('/api', (c) => {
+  // console.log('in the server side.');
+  return c.json({ message: 'Hello World!' });
+});
+
+// Denoでサーバーとして起動
+Deno.serve(app.fetch);
